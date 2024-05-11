c24053f8b16e1e7e63c3f708f596242cfa2d0a52
老房东 <huangdong@gmail.com>
2024-05-11 15:26:09 -0400

    tested ai call

---
+++ AmourIncredible/src/test/java/one/hdcola/AmourIncredible/AmourIncredibleApplicationTests.java
@@ -0,0 +1,13 @@
+package one.hdcola.AmourIncredible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmourIncredibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
