a014da4818a5a245f2b22489ce2b330516383ee3
Allenyzh <allenyangzhenhao@gmail.com>
2024-05-12 07:50:06 -0400

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
+import Chat from "./pages/Chat";
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/result" element={<Result />}></Route>
+          <Route path="/chat" element={<Chat />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
