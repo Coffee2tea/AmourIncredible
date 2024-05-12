6a2b0270f31b8f40bf7d62bc7de91e2c82e7006a
老房东 <huangdong@gmail.com>
2024-05-11 20:19:55 -0400

    use router to different page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,38 +1,16 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
-interface Scenario {
-  id: number;
-  name: string;
-  description: string;
-  prompt: string;
-}
-
-function App() {
-  const [modes, setModes] = useState<Scenario[]>([]);
-
-  useEffect(() => {
-    // 从API获取数据
-    fetch("http://100.89.152.5:8080/api/scenarios")
-      .then((response) => response.json())
-      .then((data: Scenario[]) => {
-        // 直接处理数组
-        console.log(data); // 打印数据以确认结构
-        setModes(data); // 设置状态为返回的数组
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
+const App = () => {
   return (
-    <div className="game-modes">
-      {modes && modes.map((modes, index) => (
-          <div className="mode-container" key={index}>
-            <div className="icon">{modes.name}</div>
-            <div className="tooltip">{modes.description}</div>
-          </div>
-        ))}
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
